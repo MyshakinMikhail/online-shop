e1acf028345aa39d93754fee9c1e2cfa338bf674
Mikhail Myshakin <myshakinm@mail.ru>
2025-10-22 19:05:52 +0300

    fix frontend: remove unnecessary files

--- frontend/src/app/layouts/MainLayout.tsx
+++ frontend/src/app/layouts/MainLayout.tsx
@@ -1,8 +1,8 @@
 import { Outlet } from "react-router-dom";
-import { useCategoryFromPath } from "../../../../shared/hooks";
-import { Header } from "../../../../widgets/Header";
-import { SubHeader } from "../../../../widgets/SubHeader";
-import type { Category } from "../../../../shared/types";
+import { useCategoryFromPath } from "../../shared/hooks";
+import type { Category } from "../../shared/types";
+import { Header } from "../../widgets/Header";
+import { SubHeader } from "../../widgets/SubHeader";
 
 export default function MainLayout() {
 	const category: Category = useCategoryFromPath();
