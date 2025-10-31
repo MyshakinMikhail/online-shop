693d6b1ccd15176f3ebd8aea7b5bfee29130d4b1
Mikhail Myshakin <myshakinm@mail.ru>
2025-10-31 18:05:45 +0300

    feat frontend: added delete card

--- frontend/src/entities/product/ui/lists/MiniProductsList/MiniProductsList.tsx
+++ frontend/src/entities/product/ui/lists/MiniProductsList/MiniProductsList.tsx
@@ -1,21 +1,39 @@
 import type { Product, ProductCardType } from "@/shared/types";
-import { Flex } from "antd";
+import { Flex, Typography } from "antd";
+import { useState } from "react";
 import { mockMiniProducts } from "../../../model/mocks";
-import BaseMiniProductCard from "../../cards/BaseMiniProductCard/BaseMiniProductCard";
+import MiniProductCard from "../../cards/MiniProductCard/MiniProductCard";
 import classes from "./MiniProductsList.module.css";
+
+const { Paragraph } = Typography;
 
 type Props = {
 	type: ProductCardType;
 };
 
 export default function MiniProductsList({ type }: Props) {
-	const products = mockMiniProducts;
+	const [products, setProducts] = useState<Product[]>(mockMiniProducts);
+
+	const handleDelete = (id: string) => {
+		setProducts((prevProducts) =>
+			prevProducts.filter((product) => product.id !== id)
+		);
+	};
 
 	return (
-		<Flex className={classes.products}>
-			{products.map((product: Product) => (
-				<BaseMiniProductCard product={product} type={type} />
-			))}
-		</Flex>
+		<>
+			{products.length === 0 ? (
+				<Paragraph>Товары не добавлены</Paragraph>
+			) : null}
+			<Flex className={classes.products}>
+				{products.map((product: Product) => (
+					<MiniProductCard
+						product={product}
+						type={type}
+						handleDelete={handleDelete}
+					/>
+				))}
+			</Flex>
+		</>
 	);
 }
