25c40f872a3cbc347efd4845242e50f1f182e4a2
Mikhail Myshakin <myshakinm@mail.ru>
2026-04-09 14:18:32 +0300

    fix config for playwrite

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -16,11 +16,11 @@ export default defineConfig({
 	/* Run tests in fes in parallel */
 	fullyParallel: true,
 	/* Fail the build on CI if you accidentally left test.only in the source code. */
-	forbidOnly: !!process.env.CI,
+	forbidOnly: true,
 	/* Retry on CI only */
-	retries: process.env.CI ? 2 : 0,
+	retries: 2,
 	/* Opt out of parallel tests on CI. */
-	workers: process.env.CI ? 1 : undefined,
+	workers: 1,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
 	reporter: "html",
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -72,8 +72,8 @@ export default defineConfig({
 
 	/* Run your local dev server before starting the tests */
 	webServer: {
-		command: "npm run dev",
-		url: "http://localhost:5173",
-		reuseExistingServer: !process.env.CI,
+		command: "npm run dev -- --host 127.0.0.1",
+		url: "http://127.0.0.1:5173",
+		reuseExistingServer: true,
 	},
 });
