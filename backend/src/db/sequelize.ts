8ae61c79da0a8aae2a992863fb6773017cce4f25
Mikhail Myshakin <myshakinm@mail.ru>
2026-06-30 15:17:51 +0300

    fix environment variables

--- backend/src/db/sequelize.ts
+++ backend/src/db/sequelize.ts
@@ -3,11 +3,11 @@ import { Sequelize } from "sequelize";
 
 dotenv.config();
 
-const isTestingStatus = process.env.IS_TESTING_STATUS;
+// const isTestingStatus = process.env.IS_TESTING_STATUS;
 
 const sequelize = new Sequelize({
-	database: isTestingStatus ? process.env.DB_NAME_TEST : process.env.DB_NAME,
-	// database: process.env.DB_NAME,
+	// database: isTestingStatus ? process.env.DB_NAME_TEST : process.env.DB_NAME,
+	database: process.env.DB_NAME,
 	username: process.env.DB_USER,
 	password: String(process.env.DB_PASSWORD || ""),
 	host: process.env.DB_HOST,
