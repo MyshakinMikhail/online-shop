8686bae3045039a21e4f7012eabe8a9f943db60f
Mikhail Myshakin <myshakinm@mail.ru>
2026-06-30 15:52:05 +0300

    added versel url in cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,7 +14,10 @@ app.use(express.json());
 
 app.use(
 	cors({
-		origin: "http://localhost:5173",
+		origin: [
+			"http://localhost:5173",
+			"https://online-shop-pykznbf8h-myshakinmikhails-projects.vercel.app/",
+		],
 		methods: ["GET", "POST", "PUT", "DELETE"],
 		allowedHeaders: ["Content-Type", "Authorization"],
 	})
